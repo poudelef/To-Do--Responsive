3086e7044e761d39e26791d9192a0768dcaab4dc
Sambhav <119846747+poudelef@users.noreply.github.com>
2024-01-20 15:35:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function addTask() {
     const newTaskInput = document.getElementById("newTaskinput");
     const tasklist = document.getElementById("Task-list");
-    const display_time = document.getElementById("times");
+
 
     if (newTaskInput.value !== "") {
         const newListItem = document.createElement("li");
-        newListItem.textContent = newTaskInput.value;
+        const textContentDiv = document.createElement("div");
+        textContentDiv.textContent = newTaskInput.value;
+        textContentDiv.style.marginBottom = "1rem";
+        
 
         // create a div to store 2 divs
         const timeContainer = document.createElement("div");
@@ -15,11 +18,13 @@ function addTask() {
         timeContainer.style.height = "30px";
         timeContainer.style.width = "20vw";
         timeContainer.style.margin = "3px";
+        timeContainer.style.flex = "1";
 
         // create a small div
         const small_time = document.createElement("div");
-        small_time.style.flex = "2";
+        small_time.style.flex = "1";
         small_time.style.transform = "scale(0)";
+        small_time.style.paddingRight = "1rem"
 
         // Displaying time inside div
 
@@ -34,21 +39,18 @@ function addTask() {
         })
 
         small_time.innerText = dateTime;
-        
+     
+    
+        // Creating hover effect using javaScript.
 
-        // create a big div
-        const big_time = document.createElement("div");
-        big_time.style.flex = ".5";
-        big_time.style.transform = "scale(1)";
-        big_time.innerText = "!!T!!";
-
-        big_time.addEventListener("mouseenter", function (){
+        newListItem.addEventListener("mouseenter", function (){
             small_time.style.transform = "scale(1)";
             small_time.style.transition =  "ease-in";
             small_time.style.transitionDuration = "0.3s"
         })
 
-        big_time.addEventListener("mouseleave", function (){
+
+        newListItem.addEventListener("mouseleave", function (){
             small_time.style.transform = "scale(0)";
             small_time.style.transition =  "ease-in";
             small_time.style.transitionDuration = "0.3s"
@@ -57,20 +59,27 @@ function addTask() {
         
 
         timeContainer.appendChild(small_time);
-        timeContainer.appendChild(big_time);
+
+
+        
+        
 
         // create remove button
         const removeButton = document.createElement("button");
+        timeContainer.style.flex = "0.5";
         removeButton.textContent = "Remove";
         removeButton.addEventListener("click", () => {
             tasklist.removeChild(newListItem);
-            display_time.removeChild(timeContainer);
+            tasklist.removeChild(timeContainer);
         });
 
         newListItem.insertBefore(timeContainer, newListItem.firstChild);
+        newListItem.append(textContentDiv);
         newListItem.appendChild(removeButton);
 
-        tasklist.appendChild(newListItem);
+        // This adds new list above the old. 
+        // If we did append then it will adds new list just below the old list.
+        tasklist.insertBefore(newListItem, tasklist.firstChild);
     } else {
         // Handle the case when the input is empty
         const newListItem = document.createElement("li");
